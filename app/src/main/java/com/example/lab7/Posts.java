12e00e33f2e52e67932a126a2e7fcd64b84a9660
Twaleha <twalehalodhi@gmail.com>
2022-08-01 11:24:33 +0500

    1-8-22

---
+++ app/src/main/java/com/example/lab7/Posts.java
@@ -0,0 +1,24 @@
+package com.example.lab7;
+
+public class Posts {
+
+        private String title;
+        private String body;
+
+        // generate Constructor
+        public Posts(String title, String body) {
+            this.title = title;
+            this.body = body;
+        }
+
+        //Generate Getters
+        public String getTitle() {
+            return title;
+        }
+
+        public String getBody() {
+            return body;
+        }
+    }
+
+
